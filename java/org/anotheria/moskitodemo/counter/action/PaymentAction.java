8f58e199419754ecf8c6cf3a707e6a7d783a4600
Leon Rosenberg <leon.rosenberg@anotheria.net>
2012-11-27 23:55:21 +0100

    minior fixes

--- java/org/anotheria/moskitodemo/counter/action/PaymentAction.java
+++ java/org/anotheria/moskitodemo/counter/action/PaymentAction.java
@@ -4,9 +4,6 @@ import net.anotheria.maf.action.Action;
 import net.anotheria.maf.action.ActionCommand;
 import net.anotheria.maf.action.ActionMapping;
 import net.anotheria.maf.bean.FormBean;
-import org.anotheria.moskitodemo.counter.PaymentCounter;
-import org.anotheria.moskitodemo.counter.PaymentCounterByParameter;
-import org.anotheria.moskitodemo.counter.PaymentCounterMethodBased;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,9 +15,9 @@ import javax.servlet.http.HttpServletResponse;
  * @since 19.11.12 13:23
  */
 public class PaymentAction implements Action {
-	private static PaymentCounter paymentCounter1 = new PaymentCounter();
-	private static PaymentCounterByParameter paymentCounter2 = new PaymentCounterByParameter();
-	private static PaymentCounterMethodBased paymentCounter3 = new PaymentCounterMethodBased();
+//	private static PaymentCounter paymentCounter1 = new PaymentCounter();
+//	private static PaymentCounterByParameter paymentCounter2 = new PaymentCounterByParameter();
+//	private static PaymentCounterMethodBased paymentCounter3 = new PaymentCounterMethodBased();
 
 	@Override
 	public void preProcess(ActionMapping actionMapping, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
@@ -30,13 +27,13 @@ public class PaymentAction implements Action {
 	public ActionCommand execute(ActionMapping actionMapping, FormBean formBean, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
 
 		//simply add some payment 1
-		paymentCounter1.cc();paymentCounter1.cc();paymentCounter1.ec();paymentCounter1.paypal();
+//		paymentCounter1.cc();paymentCounter1.cc();paymentCounter1.ec();paymentCounter1.paypal();
 
 		//2
-		paymentCounter2.pay("cc", true);paymentCounter2.pay("cc", true);paymentCounter2.pay("ec", true);paymentCounter2.pay("paypal", true);
-		paymentCounter2.pay("cc", false);//one failed payment
+//		paymentCounter2.pay("cc", true);paymentCounter2.pay("cc", true);paymentCounter2.pay("ec", true);paymentCounter2.pay("paypal", true);
+//		paymentCounter2.pay("cc", false);//one failed payment
 
-		paymentCounter3.pay("cc");paymentCounter3.pay("cc");paymentCounter3.pay("ec");paymentCounter3.pay("paypal");paymentCounter3.pay("paypal");
+//		paymentCounter3.pay("cc");paymentCounter3.pay("cc");paymentCounter3.pay("ec");paymentCounter3.pay("paypal");paymentCounter3.pay("paypal");
 
 		return null;  //To change body of implemented methods use File | Settings | File Templates.
 	}
