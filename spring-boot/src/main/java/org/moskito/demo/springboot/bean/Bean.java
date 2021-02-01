e9e5ccb4ef3c5962dea9db41aa1d49d746409c81
volkovdenis <volkovdenis1999@gmail.com>
2021-02-01 12:04:03 +0200

    issue-238. Added spring boot example

---
+++ spring-boot/src/main/java/org/moskito/demo/springboot/bean/Bean.java
@@ -0,0 +1,14 @@
+package org.moskito.demo.springboot.bean;
+
+public class Bean {
+
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
