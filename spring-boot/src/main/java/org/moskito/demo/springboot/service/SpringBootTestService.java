476543842bc7fdfa9953131588218324984c897d
Leon Rosenberg <leon.rosenberg@anotheria.net>
2021-02-07 19:40:32 +0100

    moved to recipes

--- spring-boot/src/main/java/org/moskito/demo/springboot/service/SpringBootTestService.java
+++
@@ -1,9 +0,0 @@
-package org.moskito.demo.springboot.service;
-
-import org.moskito.demo.springboot.bean.Bean;
-
-public interface SpringBootTestService {
-
-    String getBeanString(Bean bean);
-
-}
